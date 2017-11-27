fb365cea4320fbbfbb0342afb293b868c3086351
harryshics <harryshi.cs@gmail.com>
2017-11-27 22:39:38 +0800

    111

--- FastDRAL_batch.m
+++ FastDRAL_batch.m
@@ -5,5 +5,5 @@ alpha_candi = 10.^(-1);
 beta_candi = 10.^(-1);
 nSelInsArr = (20:20:200);
 
-dataset = 'COIL20_1440n_1024d_20c';
+dataset = 'USPS_9298n_256d_10c';
 f = FastDRAL_single(dataset,nSelInsArr,alpha_candi,beta_candi);
