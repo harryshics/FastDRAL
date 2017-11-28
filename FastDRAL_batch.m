5e1c19525403e1433e2616a1c4df104251a64f6c
harryshics <harryshi.cs@gmail.com>
2017-11-28 11:20:10 +0800

    update

--- FastDRAL_batch.m
+++ FastDRAL_batch.m
@@ -5,14 +5,17 @@ alpha_candi = 10.^(-1);
 beta_candi = 10.^(-1);
 nSelInsArr = (20:20:200);
 
-dataset = 'MNIST_10000n_784d_10c';
-f = FastDRAL_single(dataset,nSelInsArr,alpha_candi,beta_candi);
+% dataset = 'MNIST_10000n_784d_10c';
+% f = FastDRAL_single(dataset,nSelInsArr,alpha_candi,beta_candi);
+% 
+% dataset = 'USPS_9298n_256d_10c';
+% f = FastDRAL_single(dataset,nSelInsArr,alpha_candi,beta_candi);
+% 
+% dataset = 'webkb_4199n_1000d_4c_tfidf';
+% f = FastDRAL_single(dataset,nSelInsArr,alpha_candi,beta_candi);
+% 
+% dataset = 'news4a_3840n_4989d_4c';
+% f = FastDRAL_single(dataset,nSelInsArr,alpha_candi,beta_candi);
 
-dataset = 'USPS_9298n_256d_10c';
-f = FastDRAL_single(dataset,nSelInsArr,alpha_candi,beta_candi);
-
-dataset = 'webkb_4199n_1000d_4c_tfidf';
-f = FastDRAL_single(dataset,nSelInsArr,alpha_candi,beta_candi);
-
-dataset = 'news4a_3840n_4989d_4c';
+dataset = 'cifar_10_data_batch_1';
 f = FastDRAL_single(dataset,nSelInsArr,alpha_candi,beta_candi);
