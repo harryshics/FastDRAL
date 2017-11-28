f2b2094976f16ba50895f503c0f57667cfcff750
harryshics <harryshi.cs@gmail.com>
2017-11-28 10:45:55 +0800

    a faster version

--- FastDRAL_batch.m
+++ FastDRAL_batch.m
@@ -7,3 +7,12 @@ nSelInsArr = (20:20:200);
 
 dataset = 'MNIST_10000n_784d_10c';
 f = FastDRAL_single(dataset,nSelInsArr,alpha_candi,beta_candi);
+
+dataset = 'USPS_9298n_256d_10c';
+f = FastDRAL_single(dataset,nSelInsArr,alpha_candi,beta_candi);
+
+dataset = 'webkb_4199n_1000d_4c_tfidf';
+f = FastDRAL_single(dataset,nSelInsArr,alpha_candi,beta_candi);
+
+dataset = 'news4a_3840n_4989d_4c';
+f = FastDRAL_single(dataset,nSelInsArr,alpha_candi,beta_candi);
