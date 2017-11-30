c009ad13e353ab5435c5bfcf4731f631e78c59d4
harryshics <harryshi.cs@gmail.com>
2017-11-30 13:06:25 +0800

    run para tune

--- FastDRAL_batch.m
+++ FastDRAL_batch.m
@@ -1,21 +1,15 @@
 addpath('../lib/code/');
 addpath('../lib/data/');
 
-alpha_candi = 10.^(-1);
-beta_candi = 10.^(-1);
+alpha_candi = 10.^(-3:1:3);
+beta_candi = 10.^(3);
 nSelInsArr = (20:20:200);
 
-% dataset = 'MNIST_10000n_784d_10c';
-% f = FastDRAL_single(dataset,nSelInsArr,alpha_candi,beta_candi);
-% % 
-% dataset = 'USPS_9298n_256d_10c';
-% f = FastDRAL_single(dataset,nSelInsArr,alpha_candi,beta_candi);
-% 
-% dataset = 'webkb_4199n_1000d_4c_tfidf';
-% f = FastDRAL_single(dataset,nSelInsArr,alpha_candi,beta_candi);
-% 
-dataset = 'news4a_3840n_4989d_4c';
+dataset = 'MNIST_10000n_784d_10c';
 f = FastDRAL_single(dataset,nSelInsArr,alpha_candi,beta_candi);
 
-% dataset = 'cifar_10_data_batch_1';
-% f = FastDRAL_single(dataset,nSelInsArr,alpha_candi,beta_candi);
+dataset = 'USPS_9298n_256d_10c';
+f = FastDRAL_single(dataset,nSelInsArr,alpha_candi,beta_candi);
+
+dataset = 'news4a_3840n_4989d_4c';
+f = FastDRAL_single(dataset,nSelInsArr,alpha_candi,beta_candi);
